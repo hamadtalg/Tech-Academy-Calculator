af780f61a38c867a78ba4d9a974d43887b83fb01
Hamad AlGhanim <hamadalghanim@nbk.com>
2024-03-17 10:15:10 +0300

    Initial Commit

---
+++ Calculator Tech Academy/Calculator_Tech_AcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_Tech_AcademyApp.swift
+//  Calculator Tech Academy
+//
+//  Created by DVP on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_Tech_AcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
